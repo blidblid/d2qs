75f4d4f0807c23345f6cbefc8e50f5d503ba6444
Albin <e.a.e.berglund@gmail.com>
2021-11-27 20:00:18 +0100

    add hint to disabled queue button

--- src/lib/components/operators/query.operators.ts
+++ src/lib/components/operators/query.operators.ts
@@ -129,6 +129,11 @@ export class QueryOperators {
     inputs: {
       label: 'Queue',
       connect: this.rx.query.queueTrigger$,
+      hint: this.rx.user.hasErrors$.pipe(
+        map((hasErrors) => {
+          return hasErrors ? 'Add preferences before queueing.' : '';
+        })
+      ),
       disabled: combineLatest([
         this.authService.isSignedIn$,
         this.rx.query.hasErrors$,
