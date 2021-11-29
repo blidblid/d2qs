33bb10bd997e216659e8ce3e19645aba8db76097
Albin <e.a.e.berglund@gmail.com>
2021-11-29 20:24:38 +0100

    add dev firebase environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   firebase: {
-    apiKey: 'AIzaSyAE2Fz-q-kDBPzll8cH_HJm6K2ru1bJPRE',
-    authDomain: 'd2queue.firebaseapp.com',
-    databaseURL: 'https://d2queue-default-rtdb.firebaseio.com',
-    projectId: 'd2queue',
-    storageBucket: 'd2queue.appspot.com',
-    messagingSenderId: '31165220750',
-    appId: '1:31165220750:web:807dc7a38cb354679446df',
+    apiKey: 'AIzaSyAkGBg44yXA4mNmIy9qxbgvdOWrMehI02Q',
+    authDomain: 'd2queue-dev.firebaseapp.com',
+    projectId: 'd2queue-dev',
+    storageBucket: 'd2queue-dev.appspot.com',
+    messagingSenderId: '102564226339',
+    appId: '1:102564226339:web:8d141f98aedbf1eae85896',
   },
   production: false,
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
